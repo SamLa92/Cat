444d68d629ab93a4c95221c291ae37034323ccf5
SamLa92 <samloveswispa@googlemail.com>
2024-10-21 18:27:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -30,12 +30,7 @@ logging.basicConfig(filename='app.log', level=logging.INFO)
 @st.cache_resource
 def load_spacy_model():
     import spacy
-    from spacy.cli import download
-    try:
-        nlp = spacy.load('en_core_web_sm')
-    except:
-        download('en_core_web_sm')
-        nlp = spacy.load('en_core_web_sm')
+    nlp = spacy.load('en_core_web_sm')
     return nlp
 
 nlp = load_spacy_model()
